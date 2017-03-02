ccc078f84bc70c28f2159dae8da0dcf93031fd6e
Joeri Bekker <joeri@maykinmedia.nl>
2017-03-02 15:18:54 +0100

    Nicer help text presentation

--- mail_editor/admin.py
+++ mail_editor/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext_lazy as _
 
 from .forms import MailTemplateForm
 from .models import MailTemplate
@@ -9,22 +11,26 @@ from .utils import variable_help_text
 class MailTemplateAdmin(admin.ModelAdmin):
     list_display = ['template_type']
     list_filter = ['template_type']
+    readonly_fields = ('get_variable_help_text', )
     form = MailTemplateForm
 
     def get_fieldsets(self, request, obj=None):
-        description = ''
-
-        if request.POST.get('template_type'):
-            description = variable_help_text(request.POST.get('template_type'))
-        if obj:
-            description = obj.get_variable_help_text()
-
         fieldset = [
-            ('', {
+            (None, {
                 'fields': [
-                    'template_type', 'remarks', 'subject', 'body'
+                    'template_type', 'subject', 'body',
                 ],
-                'description': description,
+            }),
+            (_('Help'), {
+                'fields': [
+                    'get_variable_help_text', 'remarks',
+                ],
             }),
         ]
         return fieldset
+
+    def get_variable_help_text(self, obj):
+        if not obj.template_type:
+            return _('Please save the template to load the variables')
+        return obj.get_variable_help_text()
+    get_variable_help_text.short_description = _('Subject variables')
