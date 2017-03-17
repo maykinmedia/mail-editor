ff0767b9d1f9be6c3fc4d12201e73e344f41aa3a
Jorik Kraaikamp <jorik.kraaikamp@maykinmedia.nl>
2017-03-17 14:36:16 +0100

    updated the manifest

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='mail_editor',
-    version='0.1.8',
+    version='0.1.9',
     license='BSD',
 
     # packaging
