2ba9309346be08415ab4ac412178a0425b166de9
Sonny Bakker <sonny871@hotmail.com>
2019-12-06 14:55:00 +0100

    Pass missing parameter to url statement

--- mail_editor/urls.py
+++ mail_editor/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from .views import TemplateVariableView
 
+# Add app_name to support django 2.0+
+app_name = "mail_editor"
+
 urlpatterns = [
     url(r'^(?P<template_type>[-\w]+)/$', TemplateVariableView.as_view(), name='template_variables'),
 ]
