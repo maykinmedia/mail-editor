885fa8ef584baace388dfc1f8901d67f8c2dd5cd
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2020-03-04 10:26:09 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='mail_editor',
-    version='0.1.11',
+    version='0.3.0',
     license='BSD',
 
     # packaging
