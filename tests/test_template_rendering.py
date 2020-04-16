70f0518bbdacaac53b175a21800c76727afe80b4
Sonny Bakker <sonny871@hotmail.com>
2020-04-16 16:09:01 +0200

    Add two tests

--- tests/test_template_rendering.py
+++ tests/test_template_rendering.py
@@ -1,3 +1,4 @@
+from tempfile import TemporaryFile
 
 from django.conf import settings
 from django.test import TestCase
@@ -17,7 +18,7 @@ CONFIG = {
     "test_template": {
         "name": _("test_template"),
         "description": _("Test description"),
-        "subject_default": _("Important message {{ id }}"),
+        "subject_default": _("Important message for {{ id }}"),
         "body_default": _("Test mail sent from testcase with {{ id }}"),
         "subject": [{"name": "id", "description": ""}],
         "body": [{"name": "id", "description": ""}],
@@ -38,19 +39,50 @@ class TemplateRenderTestCase(TestCase):
     def test_simple(self):
         settings.MAIL_EDITOR_TEMPLATES = CONFIG.copy()
 
-        subject_context = {"id": "420"}
-        body_context = {"id": "420"}
+        subject_context = {"id": "111"}
+        body_context = {"id": "111"}
+
         template = find_template("test_template")
 
         subject, body = template.render(
             body_context, subj_context=subject_context
         )
 
-        self.assertEquals(subject, "Important message 420")
-        self.assertIn("Test mail sent from testcase with 420", body)
+        self.assertEquals(subject, "Important message for 111")
+        self.assertIn("Test mail sent from testcase with 111", body)
 
     def test_incorrect_base_path(self):
-        pass
+        settings.MAIL_EDITOR_TEMPLATES = CONFIG.copy()
+
+        subject_context = {"id": "222"}
+        body_context = {"id": "222"}
+
+        template = find_template("test_template")
+        template.base_template_path = b''
+
+        subject, body = template.render(
+            body_context, subj_context=subject_context
+        )
+
+        self.assertEquals(subject, "Important message for 222")
+        self.assertIn("Test mail sent from testcase with 222", body)
 
     def test_base_template_errors(self):
-        pass
+        settings.MAIL_EDITOR_TEMPLATES = CONFIG.copy()
+
+        subject_context = {"id": "333"}
+        body_context = {"id": "333"}
+
+        with TemporaryFile("w") as file:
+            file.write("Template with errors {{ id }")
+            file.seek(0)
+
+            template = find_template("test_template")
+            template.base_template_path = file.name
+
+            subject, body = template.render(
+                body_context, subj_context=subject_context
+            )
+
+            self.assertEquals(subject, "Important message for 333")
+            self.assertIn("Test mail sent from testcase with 333", body)
